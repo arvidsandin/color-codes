2f9a00097ff6b7bb80d461dc03b04cfb4c4e03e6
Arvid <arvid.sandin@gmail.com>
2020-06-25 11:15:19 +0200

    Make tones clickable Fix indentation

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ function changeBackgroundColor(color){
     $('.tint_name')[0].innerHTML = color;
     $('.shade_name')[0].innerHTML = color;
     if (isLightColor(color)){
-        $('.tint_name')[0].style.color = ('#000');
-        $('.shade_name')[0].style.color = ('#000');
-        $('.tone_name').css('color', '#000');
+      $('.tint_name')[0].style.color = ('#000');
+      $('.shade_name')[0].style.color = ('#000');
+      $('.tone_name').css('color', '#000');
     }
     else {
       $('.tint_name')[0].style.color = ('#fff');
@@ -67,7 +67,7 @@ function isLightColor(color){
 
 function hex(x) {
   if (x < 0 || x > 255) {
-      console.warn("Faulty conversion from decimal to hex");
+    console.warn("Faulty conversion from decimal to hex");
   }
   return ("0" + parseInt(x).toString(16)).slice(-2);
 }
@@ -77,62 +77,62 @@ function rgb2hex(rgb) {
 }
 
 function applySaturationToHexColor(hex, saturationPercent) {
-    if (!/^#([0-9a-f]{6})$/i.test(hex)) {
-        throw('Unexpected color format');
-    }
+  if (!/^#([0-9a-f]{6})$/i.test(hex)) {
+    throw('Unexpected color format');
+  }
 
-    if (saturationPercent < 0 || saturationPercent > 100) {
-        throw('Unexpected color format');
-    }
+  if (saturationPercent < 0 || saturationPercent > 100) {
+    throw('Unexpected color format');
+  }
 
-    var saturationFloat   = saturationPercent / 100,
-        rgbIntensityFloat = [
-            parseInt(hex.substr(1,2), 16) / 255,
-            parseInt(hex.substr(3,2), 16) / 255,
-            parseInt(hex.substr(5,2), 16) / 255
-        ];
+  var saturationFloat   = saturationPercent / 100,
+  rgbIntensityFloat = [
+    parseInt(hex.substr(1,2), 16) / 255,
+    parseInt(hex.substr(3,2), 16) / 255,
+    parseInt(hex.substr(5,2), 16) / 255
+  ];
 
-    var rgbIntensityFloatSorted = rgbIntensityFloat.slice(0).sort(function(a, b){ return a - b; }),
-        maxIntensityFloat       = rgbIntensityFloatSorted[2],
-        mediumIntensityFloat    = rgbIntensityFloatSorted[1],
-        minIntensityFloat       = rgbIntensityFloatSorted[0];
+  var rgbIntensityFloatSorted = rgbIntensityFloat.slice(0).sort(function(a, b){ return a - b; }),
+  maxIntensityFloat       = rgbIntensityFloatSorted[2],
+  mediumIntensityFloat    = rgbIntensityFloatSorted[1],
+  minIntensityFloat       = rgbIntensityFloatSorted[0];
 
-    if (maxIntensityFloat == minIntensityFloat) {
-        // All colors have same intensity, which means
-        // the original color is gray, so we can't change saturation.
-        return hex;
-    }
+  if (maxIntensityFloat == minIntensityFloat) {
+    // All colors have same intensity, which means
+    // the original color is gray, so we can't change saturation.
+    return hex;
+  }
 
-    // New color max intensity wont change. Lets find medium and weak intensities.
-    var newMediumIntensityFloat,
-        newMinIntensityFloat = maxIntensityFloat * (1 - saturationFloat);
+  // New color max intensity wont change. Lets find medium and weak intensities.
+  var newMediumIntensityFloat,
+  newMinIntensityFloat = maxIntensityFloat * (1 - saturationFloat);
 
-    if (mediumIntensityFloat == minIntensityFloat) {
-        // Weak colors have equal intensity.
-        newMediumIntensityFloat = newMinIntensityFloat;
-    }
-    else {
-        // Calculate medium intensity with respect to original intensity proportion.
-        var intensityProportion = (maxIntensityFloat - mediumIntensityFloat) / (mediumIntensityFloat - minIntensityFloat);
-        newMediumIntensityFloat = (intensityProportion * newMinIntensityFloat + maxIntensityFloat) / (intensityProportion + 1);
-    }
+  if (mediumIntensityFloat == minIntensityFloat) {
+    // Weak colors have equal intensity.
+    newMediumIntensityFloat = newMinIntensityFloat;
+  }
+  else {
+    // Calculate medium intensity with respect to original intensity proportion.
+    var intensityProportion = (maxIntensityFloat - mediumIntensityFloat) / (mediumIntensityFloat - minIntensityFloat);
+    newMediumIntensityFloat = (intensityProportion * newMinIntensityFloat + maxIntensityFloat) / (intensityProportion + 1);
+  }
 
-    var newRgbIntensityFloat       = [],
-        newRgbIntensityFloatSorted = [newMinIntensityFloat, newMediumIntensityFloat, maxIntensityFloat];
+  var newRgbIntensityFloat       = [],
+  newRgbIntensityFloatSorted = [newMinIntensityFloat, newMediumIntensityFloat, maxIntensityFloat];
 
-    // We've found new intensities, but we have then sorted from min to max.
-    // Now we have to restore original order.
-    rgbIntensityFloat.forEach(function(originalRgb) {
-        var rgbSortedIndex = rgbIntensityFloatSorted.indexOf(originalRgb);
-        newRgbIntensityFloat.push(newRgbIntensityFloatSorted[rgbSortedIndex]);
-    });
+  // We've found new intensities, but we have then sorted from min to max.
+  // Now we have to restore original order.
+  rgbIntensityFloat.forEach(function(originalRgb) {
+    var rgbSortedIndex = rgbIntensityFloatSorted.indexOf(originalRgb);
+    newRgbIntensityFloat.push(newRgbIntensityFloatSorted[rgbSortedIndex]);
+  });
 
-    var floatToHex = function(val) { return ('0' + Math.round(val * 255).toString(16)).substr(-2); },
-        rgb2hex    = function(rgb) { return '#' + floatToHex(rgb[0]) + floatToHex(rgb[1]) + floatToHex(rgb[2]); };
+  var floatToHex = function(val) { return ('0' + Math.round(val * 255).toString(16)).substr(-2); },
+  rgb2hex    = function(rgb) { return '#' + floatToHex(rgb[0]) + floatToHex(rgb[1]) + floatToHex(rgb[2]); };
 
-    var newHex = rgb2hex(newRgbIntensityFloat);
+  var newHex = rgb2hex(newRgbIntensityFloat);
 
-    return newHex;
+  return newHex;
 }
 
 $(document).ready(function() {
@@ -164,8 +164,8 @@ $(document).ready(function() {
     changeBackgroundColor('#' + rgb[0] + rgb[1] + rgb[2]);
   });
 
-  $('.tint, .shade').click(function(){
-    var chosenColor = rgb2hex($(this).css('backgroundColor'))
+  $('.tint, .shade, .tone').click(function(){
+    var chosenColor = rgb2hex($(this).css('backgroundColor'));
     $('#hex_input').val(chosenColor);
     changeBackgroundColor(chosenColor);
   });
